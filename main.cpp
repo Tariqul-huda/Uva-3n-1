d091b1f25a0cc53b867e20b907f571b104dfb2fc
Tariqul Huda <tariqulhuda6@gmail.com>
2022-12-25 19:13:41 +0600

    My first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int countNumber(int i,int j);
+int main()
+{
+    int i,j;
+    freopen("number.txt","r",stdin);
+    while(scanf("%d %d",&i,&j)==2)
+    {
+        cout<<i<<" "<<j<<" "<<countNumber(i,j)<<endl;
+    }
+    return 0;
+}
+
+int countNumber(int small,int large)
+{
+     int maximum=0;
+     int t=0;
+    for( int temp=large; temp>=small; temp--)
+    {
+         int c=1;
+        t=temp;
+        while(t!=1)
+        {
+            if(t%2!=0)
+            {
+                t=3*t+1;
+            }
+            else
+                t=t/2;
+            c++;
+        }
+        if(c>maximum)
+            maximum=c;
+    }
+
+    return maximum;
+
+}
